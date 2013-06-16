747cca642440305122027f25bfad7601d2b4395e
Isaac Hogue <isaachogue@gmail.com>
2013-06-16 09:53:26 -0400

    Adding solution to github

---
+++ SabreExcelImport/SymphonyApi/IAccessManagerInteractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Iformata.Vnoc.Symphony.Enterprise.Data.InformationModel.ServiceTypes;
+
+namespace AviSpl.Vnoc.Symphony.Services.Api
+{
+    public interface IAccessManagerInteractor
+    {
+        SessionToken Login(Dictionary<string, string> credentials);
+        SessionToken LoginWithCredentials(Credential credentials);
+        bool Logout(string SessionId);
+        Session Authenticate(string SessionId);
+    }
+}
