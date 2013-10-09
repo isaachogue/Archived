096947a5d7721fe34461e39c2c1a6c985715dd07
Isaac Hogue <isaachogue@gmail.com>
2013-10-09 06:59:10 -0400

    Cleaning up the google sync project
    removing code that may not be required
    creating very focused spec for google sync

---
+++ GoogleSync/Spec/Discover_Google_Calendar_Events.feature
@@ -0,0 +1,14 @@
+﻿Feature: Sync Meetings From Google - Discover Google Calendar Events
+	As the VNNOC Symphony Cloud Services
+	I want to be notified when a managed space is scheduled for a meeting through google calendar
+	So that I can maintain an accurate understanding of the the space's scheduled meetings
+
+Scenario: Inform google calendar to notifiy Symphony of events for a managed space
+	Given Symphony has a valid Google API key
+	And a managed space has been selected for google sync
+	When Symphony makes a watch request for the space
+	And the watch request includes an id property
+	And the watch request includes a type property
+	And the watch request includes a URI where requests should be provided
+	And the watch request includes a token property with the space GUID
+	Then the result should be an HTTP 200 OK status code
--- GoogleSync/Spec/Properties/AssemblyInfo.cs
+++ GoogleSync/Spec/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spec")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GoogleSyncSpec")]
+[assembly: AssemblyDescription("Specification of features that should be in place to support google calendar synchronization for Symphony spaces")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Spec")]
+[assembly: AssemblyProduct("GoogleSyncSpec")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("13.1.*")]
